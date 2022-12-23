b6450fd7dd77d68dea5cd4613e8b55a3f61427be
Vijayabalan <bvijaycom@gmail.com>
2022-12-23 19:16:27 +0100

    class3

--- week10-class3/03-import-existing-infra/ec2.tf
+++ week10-class3/03-import-existing-infra/ec2.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "myvm" {
- ami           = "ami-0b5eea76982371e91"
- instance_type = "t2.micro"
+resource "aws_instance" "myvm1" {
+  ami           = "XXXX"
+  instance_type = "t2.micro"
 }
 
 resource "aws_instance" "myvm2" {
- ami           = "ami-0b5eea76982371e91"
- instance_type = "t2.micro"
+  ami           = "XXXXX"
+  instance_type = "t2.micro"
 }
